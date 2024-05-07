2208b84d671c36e73ae676f543334e3ef19a0e7c
thuctran-smf <thuc.tran@sanoma.com>
2024-05-07 16:52:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,13 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
+        
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
     #st.write(ingredient_string)
 
