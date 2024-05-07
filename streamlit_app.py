4983a314565264dad6e4b7e4fd5fc1aa8ea8874e
thuctran-smf <thuc.tran@sanoma.com>
2024-05-07 16:06:17 +0300

    Update streamlit_app.py
    Optimizing

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
-        st.subheader(fruit + 'Nutrition Information')
+        st.subheader(fruit + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
     #st.write(ingredient_string)
